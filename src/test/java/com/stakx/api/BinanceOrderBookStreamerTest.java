6cf5eb7f4fd42e1dae950abde54909c2ccd0a045
nithinmurali <imnmfotmal@gmail.com>
2019-07-05 10:45:21 +0530

    added basic test layout

---
+++ src/test/java/com/stakx/api/BinanceOrderBookStreamerTest.java
@@ -0,0 +1,11 @@
+package com.stakx.api;
+
+import org.junit.Test;
+
+public class BinanceOrderBookStreamerTest {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+
+    }
+}
